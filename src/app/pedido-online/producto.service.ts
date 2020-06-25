133e18656c55afdea37cb21707348cb6881857d5
alex10ronda <alex10_ronda@hotmail.com>
2020-06-25 18:33:25 +0200

    Creacion del servicio

---
+++ src/app/pedido-online/producto.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from 'rxjs';
+
+import { Producto } from "./modelo/producto.model";
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductoService {
+
+  private basePath = "http://127.0.0.1:8080/el-lechuguita/productos";
+  private pathListado = "/listaProductos";
+
+  constructor(private http: HttpClient) { }
+
+  getAllProductos():Observable<Producto>{
+    return this.http.get(this.basePath + this.pathListado);
+  }
+}
